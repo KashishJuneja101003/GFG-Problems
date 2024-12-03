0d96b16bf79a80c7d9c5d07880dd1eba90818660
Kashish Juneja <kashishjuneja1010@gmail.com>
2024-12-03 17:55:29 +0530

    Create if-else (Decision Making).cpp

---
+++ if-else (Decision Making).cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    string compareNM(int n, int m){
+        // code here
+        if(n == m) return "equal";
+        else if (n < m) return "lesser";
+        else return "greater";
+    }
+};
