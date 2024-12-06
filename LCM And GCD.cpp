74a55b5e267dd619b5b4cbb3b64dad1711ae6855
Kashish Juneja <kashishjuneja1010@gmail.com>
2024-12-06 11:35:43 +0530

    Efficient

--- LCM And GCD.cpp
+++ LCM And GCD.cpp
@@ -2,14 +2,13 @@ class Solution {
   public:
     vector<int> lcmAndGcd(int a, int b) {
         // code here
-        if(a==1 || b==1) return {a*b, 1};
         
         int lcm, gcd=1;
-        for(int i=2; i <= a; i++){
+        for(int i=2; i <= min(a, b); i++){
             if(a%i == 0 && b%i==0) gcd = i;
         }
         
-        lcm = (a*b)/gcd;
+        lcm = (a/gcd)*b; //To avoid overflow
         
         return {lcm, gcd};
     }
