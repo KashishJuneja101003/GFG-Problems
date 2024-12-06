589c9373bef5bc872b1cf541c37b1c7dd5aa84f9
Kashish Juneja <kashishjuneja1010@gmail.com>
2024-12-06 11:38:25 +0530

    Efficient

--- LCM And GCD.cpp
+++ LCM And GCD.cpp
@@ -1,15 +1,27 @@
+#include <vector>
+#include <algorithm> // for std::gcd
+using namespace std;
+
 class Solution {
   public:
     vector<int> lcmAndGcd(int a, int b) {
-        // code here
-        
-        int lcm, gcd=1;
-        for(int i=2; i <= min(a, b); i++){
-            if(a%i == 0 && b%i==0) gcd = i;
-        }
-        
-        lcm = (a/gcd)*b; //To avoid overflow
-        
+        // Compute GCD using Euclid's algorithm (or STL function)
+        int gcd = computeGCD(a, b);
+
+        // Compute LCM using the relationship with GCD
+        int lcm = (a / gcd) * b; // Division first to avoid overflow
+
         return {lcm, gcd};
+    }
+
+  private:
+    // Function to compute GCD using Euclid's algorithm
+    int computeGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 };
