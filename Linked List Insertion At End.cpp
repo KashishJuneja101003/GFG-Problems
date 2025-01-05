704de0cdb31757e9681648390a398a155811d1e3
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-01-05 23:01:21 +0530

    Create Linked List Insertion At End.cpp

---
+++ Linked List Insertion At End.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        // Code here
+        Node* newNode = new Node(x);
+        
+        if(head == nullptr){
+            head = newNode;
+            return head;
+        }
+        
+        Node* temp = head;
+        
+        while(temp->next != nullptr){
+            temp = temp->next;
+        }
+        
+        temp->next = newNode;
+        
+        return head;
+    }
+};
