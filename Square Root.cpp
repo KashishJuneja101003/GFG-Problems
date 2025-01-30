83b066f878e337e80431d77363157822918268fe
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-01-30 09:11:56 +0530

    Create Square Root.cpp

---
+++ Square Root.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        int start = 1, end = n;
+        int ans;
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            if(mid*mid <= n){
+                ans = mid;
+                start = mid+1;
+            } else{
+                end = mid-1;
+            }
+        }
+        
+        return ans;
+    }
+};
