968b60f2486a1f540044470064c138221048439c
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-02-04 08:00:29 +0530

    Update Stack using Linked List.cpp

--- Stack using Linked List.cpp
+++ Stack using Linked List.cpp
@@ -1,31 +1,23 @@
 class MyStack {
   private:
     StackNode *top;
-    
-    struct Node{
-        int data;
-        Node* next;
-    };
-    
-    Node* head = nullptr;
 
   public:
     void push(int x) {
         // code here
-        Node* newNode = new Node();
-        newNode->data = x;
-        newNode->next = head;
-        head = newNode;
+        StackNode* newNode = new StackNode(x);
+        newNode->next = top;
+        top = newNode;
     }
 
     int pop() {
         // code here
-        if(head == nullptr){
+        if(top == nullptr){
             return -1;
         }
         
-        int temp = head->data;
-        head = head->next;
+        int temp = top->data;
+        top = top->next;
         return temp;
     }
 
