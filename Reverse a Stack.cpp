dbd8e85519558de3802b6e001e34b28295b8dec0
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-02-18 06:44:24 +0530

    Create Reverse a Stack.cpp

---
+++ Reverse a Stack.cpp
@@ -0,0 +1,24 @@
+class Solution{
+public:
+    void insertAtBottom(stack<int> &St, int element){
+        if(St.empty()){
+            St.push(element);
+            return;
+        }
+        
+        int top = St.top();
+        St.pop();
+        insertAtBottom(St, element);
+        St.push(top);
+    }
+    void Reverse(stack<int> &St){
+        if(St.empty()){
+            return;
+        }
+        
+        int top = St.top();
+        St.pop();
+        Reverse(St);
+        insertAtBottom(St, top);
+    }
+};
