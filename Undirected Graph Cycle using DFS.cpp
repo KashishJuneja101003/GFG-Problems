132dda765cd919029ce2d456ff982a2d648c5865
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-05-23 16:39:13 +0530

    Updated code with comments to dry run effectively

--- Undirected Graph Cycle using DFS.cpp
+++ Undirected Graph Cycle using DFS.cpp
@@ -1,22 +1,53 @@
 class Solution {
   public:
-    bool DFS_Cycle_Check(vector<vector<int>>&adj, int u, vector<bool> &visited, int parent){
+    bool detectCycleDFS(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited, int parent){
         visited[u] = true;
-        for(int &v:adj[u]){
-            if(v == parent) continue;
-            if(visited[v] == true) return true;
-            if(DFS_Cycle_Check(adj, v, visited, u)) return true;
+        // cout<<"\n Visited Now: "<<u;
+        
+        for(int &v : adj[u]){
+            // cout<<"\n Checking for node: "<<v;
+            
+            // Don't revisit the parent node
+            if(v == parent){
+                // cout<<"\n Parent Node: "<<v;
+                continue;
+            }
+            
+            // Cycle detected
+            if(visited[v] == true){
+                // cout<<"\n Node responsible for making cycle: "<<u;
+                return true;
+            }
+            
+            // Recurse, return if cycle detected at any point
+            if(detectCycleDFS(adj, v, visited, u)){
+                // cout<<"\n Cycle Detected (u, parent): "<<v <<", "<<u;
+                return true;
+            }
         }
+        
+        // No cycle found
         return false;
     }
-    
-    bool isCycle(vector<vector<int>>& adj) {
-        int n = adj.size();
-        vector<bool> visited(n, false);
+  
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        for(auto vec : edges){
+            int u = vec[0];
+            int v = vec[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
         
-        for(int i=0; i<n; i++){
-            if(!visited[i] && DFS_Cycle_Check(adj, i, visited, -1)){
-                return true;
+        vector<bool> visited(V, false);
+        
+        // Check all components (graph might be disconnected)
+        for(int i=0; i<V; i++){
+            if(!visited[i]){
+                // cout<<"\n Checking for node: "<<i;
+                if(detectCycleDFS(adj, i, visited, -1)){
+                    return true;
+                }
             }
         }
         
