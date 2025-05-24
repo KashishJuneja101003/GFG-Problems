a6a1c20c1dfa207af72015a80e92c5cf78e4349b
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-05-24 19:51:39 +0530

    Updated code with comments to dry run effectively

--- Undirected Graph Cycle using BFS.cpp
+++ Undirected Graph Cycle using BFS.cpp
@@ -1,45 +1,55 @@
 class Solution {
   public:
-    bool isCycleBFS(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited){
-        queue<pair<int, int>> que;
-        
-        que.push({u, -1});
+    bool detectCycleBFS(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited, int parent){
         visited[u] = true;
         
-        while(!que.empty()){
-            auto P = que.front();
-            que.pop();
+        queue<pair<int, int>> node_parent;
+        node_parent.push({u, parent});
+        
+        while(!node_parent.empty()){
+            int n = node_parent.front().first;
+            int p = node_parent.front().second;
+            node_parent.pop();
             
-            int src = P.first, parent = P.second;
-            
-            for(int &v : adj[src]){
-                if(visited[v] == false){
-                    visited[v] = true;
-                    que.push({v, src});
-                } else if(v != parent){
+            for(int &v : adj[n]){
+                if(v == p){
+                    continue;
+                }
+                
+                if(visited[v] == true){
+                    return true;
+                }
+                
+                node_parent.push({v, n});
+                visited[v] = true;
+            }
+        }
+        
+        return false;
+        
+    }
+  
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        for(auto vec : edges){
+            int u = vec[0];
+            int v = vec[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<bool> visited(V, false);
+        
+        // Check all components (graph might be disconnected)
+        for(int i=0; i<V; i++){
+            if(!visited[i]){
+                // cout<<"\n Checking for node: "<<i;
+                if(detectCycleBFS(adj, i, visited, -1)){
                     return true;
                 }
             }
         }
         
         return false;
-    }
-    
-    bool isCycle(int V, vector<vector<int>>& edges) {
-        unordered_map<int, vector<int>> adj;
-        for(int i=0; i<edges.size(); i++){
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
-        }
-        
-        vector<bool> visited(V, false);
-        for(int i=0; i<V; i++){
-            if(!visited[i] && isCycleBFS(adj, i, visited)){
-                return true;
-            }
-        }
-        
-        return false;
-        
     }
 };
