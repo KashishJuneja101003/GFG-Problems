79b714b9b0212a10aa56e928a17ee80957a83075
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-05-25 17:00:12 +0530

    Updated code with comments to dry run effectively

--- Cycle in a Directed Graph using DFS.cpp
+++ Cycle in a Directed Graph using DFS.cpp
@@ -1,30 +1,45 @@
 class Solution {
   public:
-    bool isCycleDFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<bool> &inRecursion){
-        visited[u] = inRecursion[u] = true;
+    bool detectCycleDFS(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited, vector<bool>& inRecursion){
+        // Mark node as visited
+        visited[u] = true; inRecursion[u] = true;
         
+        // Explore the neighbors of u
         for(int &v : adj[u]){
-            if(visited[v] == false && isCycleDFS(adj, v, visited, inRecursion))
-                return true;
-            else if (inRecursion[v] == true)
-                return true;
+            // If the node 'v' is present in current recursion and visited again, it means a cycle is formed
+            if(inRecursion[v] == true) return true;
+            
+            // If the subsequent recursion finds a cycle, true is returned
+            // Calling detectCycleDFS function, only if the node is not visited earlier to avoid unnecessary recursive calls
+            if(!visited[v] && detectCycleDFS(adj, v, visited, inRecursion)) return true;
         }
         
+        // Mark the node as unvisited in inRecursion bcz it is no longer a part of the current recursion (going to occur next)
         inRecursion[u] = false;
+        
+        // If no cycle is detected, return false
         return false;
     }
-    
-    bool isCyclic(vector<vector<int>> &adj) {
-        int V = adj.size();
+  
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        // Creating adjacency list
+        unordered_map<int, vector<int>> adj;
+        for(auto vec : edges){
+            int u = vec[0];
+            int v = vec[1];
+            
+            adj[u].push_back(v);
+        }
         
+        // visited keeps track of visited nodes as a whole
+        // inRecursion keeps track of the nodes visited in current recursion
+        vector<bool> visited(V, false);
+        vector<bool> inRecursion(V, false);
         
-        
-        vector<bool> visited (V, false), inRecursion (V, false);
-        
+        // Calling DFS in a loop bcz the graph may be disconnected
         for(int i=0; i<V; i++){
-            if(!visited[i] && isCycleDFS(adj, i, visited, inRecursion)){
-                return true;
-            }
+            if(visited[i]) continue;
+            if(detectCycleDFS(adj, i, visited, inRecursion)) return true;
         }
         
         return false;
