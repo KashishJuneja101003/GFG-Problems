b284f228040b5993e4fc4da3dd9f32d0205aca58
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-08 16:53:27 +0530

    Update Minimum element in BST.cpp

--- Minimum element in BST.cpp
+++ Minimum element in BST.cpp
@@ -1,12 +1,9 @@
+// Approach 1: TC: O(h) SC: O(1)
 class Solution {
   public:
     int minValue(Node* root) {
-        // Code here
         Node* temp = root;
-        while(temp->left != nullptr){
-            temp = temp->left;
-        }
-        
+        while(temp->left) temp = temp->left;
         return temp->data;
     }
 };
