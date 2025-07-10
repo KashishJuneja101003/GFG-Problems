941cd74d481979a25e77bd4d7425bc0fbaae96d7
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-10 19:31:30 +0530

    Update DFS of Graph.cpp

--- DFS of Graph.cpp
+++ DFS of Graph.cpp
@@ -1,32 +1,20 @@
 class Solution {
   public:
-    void DFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<int>& DFSOrder){
-        if(visited[u] == true){
-            // cout<<"\n "<<u<<" is visited";
-            return;
-        }
-        
+    void calcDFS(vector<vector<int>>& adj, vector<int>& order, vector<bool>& visited, int u){
         visited[u] = true;
-        DFSOrder.push_back(u);
-        // cout<<"\n "<<u<<" visited now";
+        order.push_back(u);
         
-        for(int &v : adj[u]){
-            DFS(adj, v, visited, DFSOrder);
-            // cout<<"\n "<<v<<" returned back";
+        for(int& v:adj[u]){
+            if(!visited[v]){
+                calcDFS(adj, order, visited, v);
+            }
         }
-        
-        // cout<<"\n "<<u<<" is over";
     }
-  
+    
     vector<int> dfs(vector<vector<int>>& adj) {
-        // Code here
-        int V = adj.size();
-        
-        vector<bool> visited(V, false);
-        vector<int> DFSOrder;
-        
-        DFS(adj, 0, visited, DFSOrder);
-        
-        return DFSOrder;
+        vector<int> order;
+        vector<bool> visited(adj.size(), false);
+        calcDFS(adj, order, visited, 0);
+        return order;
     }
 };
