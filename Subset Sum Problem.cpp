50f68029762787e1df73c47aa2119156f15684f8
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-18 10:41:06 +0530

    Update Subset Sum Problem.cpp

--- Subset Sum Problem.cpp
+++ Subset Sum Problem.cpp
@@ -1,27 +1,45 @@
-// Approach 1: Recursion O(2^n)
+// Recursive: TC: O(2^n)
 class Solution {
   public:
-    bool func(vector<int>& arr, int target, int ind, int sum){
-        if(ind < 0) {return sum == target;}
+    bool fun(vector<int>& arr, int currSum, int n, int target){
+        if(n == 0) return false;
+        if(currSum == target) return true;
         
-        if(arr[ind] > target) {
-            return func(arr, target, ind-1, sum);
-        }
-        else if(arr[ind] == target || sum == target) {
-            return true;
-        }
-        else{
-            bool exclude = func(arr, target, ind-1, sum);
-            bool include = func(arr, target, ind-1, sum + arr[ind]);
-            
-            return include || exclude;
-        }
+        // Include
+        bool include = fun(arr, currSum + arr[n-1], n-1, target);
         
+        // Exclude
+        bool exclude = fun(arr, currSum, n-1, target);
+        
+        return include || exclude;
     }
-    bool isSubsetSum(vector<int>& arr, int target) {
-        return func(arr, target, arr.size()-1, 0);
+    
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        return fun(arr, 0, arr.size(), sum);
     }
 };
 
+// Memoized TC: O(n*target)
+class Solution {
+public:
+    bool fun(vector<int>& arr, int n, int target, vector<vector<int>>& dp) {
+        if (target == 0) return true;
+        if (n == 0) return false;
+        
+        // Memoization check
+        if (dp[n][target] != -1) return dp[n][target];
+        
+        bool exclude = fun(arr, n - 1, target, dp);
+        bool include = false;
+        if (arr[n - 1] <= target)
+            include = fun(arr, n - 1, target - arr[n - 1], dp);
+        
+        return dp[n][target] = include || exclude;
+    }
 
-// Approach 2: DP
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, -1));
+        return fun(arr, n, sum, dp);
+    }
+};
