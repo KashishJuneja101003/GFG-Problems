00f7bfe94a4baf76b12eadc049f806b171a888b4
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-25 18:23:25 +0530

    Update Frog Jump.cpp

--- Frog Jump.cpp
+++ Frog Jump.cpp
@@ -17,3 +17,28 @@ class Solution {
         return helper(height, 0);
     }
 };
+
+// Memoized TC: O(n) SC: O(n)
+class Solution {
+  public:
+    int helper(vector<int>& height, int idx, int n, vector<int>& dp){
+        if(idx == n-1) return 0;
+        
+        if(dp[idx] != -1) return dp[idx];
+        
+        int oneStep = helper(height, idx+1, n, dp) + abs(height[idx+1] - height[idx]);
+        
+        int twoStep = INT_MAX;
+        
+        if(idx+2 < height.size())
+            twoStep = helper(height, idx+2, n, dp) + abs(height[idx+2] - height[idx]);
+        
+        return dp[idx] = min(oneStep, twoStep);
+    }
+    int minCost(vector<int>& height) {
+        int n = height.size();
+        
+        vector<int> dp(n, -1);
+        return helper(height, 0, n, dp);
+    }
+};
