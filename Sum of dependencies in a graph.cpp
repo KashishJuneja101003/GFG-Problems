61dda584b13bb688af1337380b5738ddbcdd5e67
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-01 15:47:19 +0530

    Create Sum of dependencies in a graph.cpp

---
+++ Sum of dependencies in a graph.cpp
@@ -0,0 +1,8 @@
+class Solution {
+  public:
+    // Function to calculate the sum of dependencies.
+    int sumOfDependencies(int V, vector<vector<int>> &edges) {
+        
+        return edges.size();
+    }
+};
