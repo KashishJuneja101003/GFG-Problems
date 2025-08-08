322d4eb9b49bc57ba3837d1c1994e0cd73bee61f
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-08 11:59:54 +0530

    Update Steps by Knight.cpp

--- Steps by Knight.cpp
+++ Steps by Knight.cpp
@@ -45,3 +45,63 @@ class Solution {
     }
 };
 
+// BFS TC: O(n^2) SC: O(n^2)
+class Solution {
+  public:
+    vector<vector<int>> directions = {{2,1}, {2,-1}, {-2,1}, {-2,-1}, {1,2}, {-1, 2}, {1, -2}, {-1, -2}};
+    
+    bool isSafe(int x, int y, int n){
+        return x >= 0 && y >= 0 && x < n && y < n;
+    }
+    
+    int getSteps(vector<int>& knightPos, vector<int>& targetPos, int n, vector<vector<bool>>& visited){
+        int xi = knightPos[0];
+        int yi = knightPos[1];
+        int xt = targetPos[0];
+        int yt = targetPos[1];
+        
+        // {steps, {x,y}}
+        queue<pair<int, pair<int, int>>> q;
+        q.push({0, {xi, yi}});
+        
+        visited[xi][yi] = true;
+        
+        while(!q.empty()){
+            auto curr = q.front();
+            q.pop();
+            
+            int steps = curr.first;
+            int x = curr.second.first;
+            int y = curr.second.second;
+            
+            // If reached target, return minSteps
+            if(x == xt && y == yt){
+                return steps;
+            }
+            
+            for(auto& dir:directions){
+                int x_ = x + dir[0];
+                int y_ = y + dir[1];
+                
+                if(isSafe(x_, y_, n) && !visited[x_][y_]){
+                    visited[x_][y_] = true;
+                    q.push({steps+1, {x_, y_}});
+                }
+            }
+        }
+        
+        return -1;
+     }
+     
+    int minStepToReachTarget(vector<int>& knightPos, vector<int>& targetPos, int n) {
+        // Convert position from 1-based to 0-based indexing
+        knightPos[0]--;
+        knightPos[1]--;
+        targetPos[0]--;
+        targetPos[1]--;
+        
+        vector<vector<bool>> visited(n, vector<bool> (n, false));
+        
+        return getSteps(knightPos, targetPos, n, visited);
+    }
+};
