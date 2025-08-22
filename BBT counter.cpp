6f40dd646124a63468750ac789fb845593f19e3a
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-22 11:11:27 +0530

    Create BBT counter.cpp

---
+++ BBT counter.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int countBT(int h) {
+        if(h <= 1) return 1;
+        vector<int> dp(h+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2; i<=h; i++){
+            dp[i] = dp[i-1]*(2*dp[i-2]+dp[i-1]);
+        }
+        
+        return dp[h];
+    }
+};
