b6673295504b72c16f599f5bbd87e52dfd3c41fe
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-25 12:50:51 +0530

    Update K-th element of two Arrays.cpp

--- K-th element of two Arrays.cpp
+++ K-th element of two Arrays.cpp
@@ -1,32 +1,39 @@
 class Solution {
   public:
-    int kthElement(vector<int>& a, vector<int>& b, int k) {
-        // code here
-        int i=0, j=0, n1=a.size(), n2=b.size(), x=0;
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
+        int i=0;
+        int j=0;
+        int n1=a.size();
+        int n2=b.size();
+        
+        int ans;
+        int idx = 0;
         
         while(i < n1 && j < n2){
-            int el;
-            if(a[i] <= b[j]){
-                el = a[i]; i++;
-            } else{
-                el = b[j]; j++;
+            if(a[i] < b[j]){
+                ans = a[i];
+                i++;
+            } else {
+                ans = b[j];
+                j++;
             }
             
-            x++;
-            
-            if(x == k) return el;
+            if(idx+1 == k) return ans;
+            idx++;
         }
         
-        while(i < n1){
-            int el = a[i];
-            i++; x++;
-            if(x == k) return el;
+        while(i < n1 && idx != k){
+            ans = a[i];
+            i++;
+            idx++;
         }
         
-        while(j < n2){
-            int el = b[j];
-            j++; x++;
-            if(x == k) return el;
+        while(j < n2 && idx != k){
+            ans = b[j];
+            j++;
+            idx++;
         }
+        
+        return ans;
     }
 };
