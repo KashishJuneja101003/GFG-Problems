0a42c59f3cd19f39e5ce8e70198729fd64d990af
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-27 08:37:52 +0530

    Update Rat in a Maze Problem - I.cpp

--- Rat in a Maze Problem - I.cpp
+++ Rat in a Maze Problem - I.cpp
@@ -1,30 +1,32 @@
 class Solution {
   public:
-    void move(int i, int j, vector<vector<int>>& maze, vector<string>& paths, string currentPath){
-        if(maze[i][j] == 0) return;
-        
-        int n = maze.size();
-        if(i == n-1 && j == n-1){
-            paths.push_back(currentPath); return;
+    int n;
+    void getOrder(vector<vector<int>>& maze, int x, int y, string path, vector<string>& ans){
+        if(x < 0 || y < 0 || x >= n || y >= n || maze[x][y] == 0){
+            return;
         }
         
-        maze[i][j] = 0;
+        if(x == n-1 && y == n-1){
+            ans.push_back(path);
+            return;
+        }
         
-        if(i-1 >= 0) move(i-1, j, maze, paths, currentPath+'U');
+        maze[x][y] = 0;
         
-        if(i+1 < n) move(i+1, j, maze, paths, currentPath+'D');
+        getOrder(maze, x+1, y, path + 'D', ans);
+        getOrder(maze, x, y-1, path + 'L', ans);
+        getOrder(maze, x, y+1, path + 'R', ans);
+        getOrder(maze, x-1, y, path + 'U', ans);
         
-        if(j-1 >= 0) move(i, j-1, maze, paths, currentPath+'L');
-        
-        if(j+1 < n) move(i, j+1, maze, paths, currentPath+'R');
-        
-        maze[i][j] = 1;
+        maze[x][y] = 1;
     }
-  
+    
     vector<string> ratInMaze(vector<vector<int>>& maze) {
-        vector<string> paths;
-        move(0, 0, maze, paths, "");
-        sort(paths.begin(), paths.end());
-        return paths;
+        n = maze.size();
+        vector<string> ans;
+        
+        getOrder(maze, 0, 0, "", ans);
+        
+        return ans;
     }
 };
